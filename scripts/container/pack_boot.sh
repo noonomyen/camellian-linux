514baf72cf950221ee5e99a2b0df81c1f36855df
noonomyen <nomyen@noonomyen.com>
2026-08-31 02:28:39 +0700

    build: Move boot.stock.img and magisk flags to images/
    - Rename boot/boot.stock.img to images/boot.stock.img
    - Rename boot/magisk-flags.txt to images/magisk-flags.txt
    - Update scripts/container/pack_boot.sh to use the new paths

--- scripts/container/pack_boot.sh
+++ scripts/container/pack_boot.sh
@@ -9,10 +9,10 @@ if [ "${IS_CONTAINER}" != "1" ]; then
 fi
 
 MODE="clean"
-INPUT_BOOT="/workspace/boot/boot.stock.img"
+INPUT_BOOT="/workspace/images/boot.stock.img"
 KERNEL_IMAGE="/workspace/out/arch/arm64/boot/Image.gz"
-CONFIG_FILE="/workspace/boot/magisk-flags.txt"
-[ ! -f "$CONFIG_FILE" ] && CONFIG_FILE="/workspace/boot/stock-magisk/flags.txt"
+CONFIG_FILE="/workspace/images/magisk-flags.txt"
+[ ! -f "$CONFIG_FILE" ] && CONFIG_FILE="/workspace/images/stock-magisk/flags.txt"
 
 while [ $# -gt 0 ]; do
     case "$1" in
